52c1ba458051751dace3e166ea09e5cee6b455f6
Kieran T Brawley <ktb.developer@gmail.com>
2020-05-29 10:45:30 +0100

    add re-spin and more guns

--- mystery-box.py
+++ mystery-box.py
@@ -1,37 +1,61 @@
-
+# External Libs
 import simpleaudio as sa
 import json
 import random
+
+# Global Variables
 quitProgram = False
 sfx_path = "./sound-effects"
+prevGun = ""
+spinCount = 0
+guns = []
 
-#====HELPER METHODS=====#
+# Helper methods
 def loadGunsFromJson(jsonPath):
     with open('guns.json', 'r') as f:
         data = json.load(f)
-        return data['guns'];
+        return data['guns']
+
 
 def playSound(path):
     wave_obj = sa.WaveObject.from_wave_file(f"{path}")
     play_obj = wave_obj.play()
     play_obj.wait_done()
 
-def spinMysteryBox():
+
+def spinMysteryBox(playAudio):
     index = random.randrange(0, len(guns))
-    playSound(f"{sfx_path}/mystery-box.wav")
-    print(guns[index]['name']);
+    if (playAudio):
+        playSound(f"{sfx_path}/mystery-box.wav")
+
+    prevGun = guns[index]['name']
+    print()
     return guns[index]
 
 
-#====MAIN=====#
+def quit():
+    global quitProgram
+    quitProgram = True
+
+
+# Main
 playSound(f"{sfx_path}/round-begin.wav")
+guns = loadGunsFromJson('./guns.json')
 
-guns = loadGunsFromJson('./guns.json');
-gun = spinMysteryBox()
+while quitProgram == False:
+    print('Would you like to spin the box?: (Y/N)')
+    answer = input()
+    print()
 
-if(gun['name'] == "Teddy Bear"):
-    playSound(f"{sfx_path}./teddy.wav")
-
-
-
-
+    if (answer == 'y' or answer == 'Y'):
+        gun = spinMysteryBox(True)
+        spinCount += 1
+        while gun['name'] == prevGun:
+            spinMysteryBox(False)
+        print(f"You've gained the {gun['name']}")
+        print()
+        if (gun['name'] == "Teddy Bear"):
+            playSound(f"{sfx_path}./teddy.wav")
+            quit()
+    else:
+        quit()
