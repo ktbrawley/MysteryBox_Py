d749c25fffd95493525fec83a43ff1ca2a18bea1
Kieran T Brawley <ktb.developer@gmail.com>
2020-05-29 15:34:45 +0100

    set randomized spinLimit and teddy spawn

--- mystery-box.py
+++ mystery-box.py
@@ -2,6 +2,8 @@
 import simpleaudio as sa
 import json
 import random
+import serial
+import time
 
 # Global Variables
 quitProgram = False
@@ -9,8 +11,19 @@ sfx_path = "./sound-effects"
 prevGun = ""
 spinCount = 0
 guns = []
+serialPort = None
 
 # Helper methods
+def readButtonState(serialPort): 
+    serialPort.flush() # it is buffering. required to get the data out *now*
+    input = serialPort.readline(100);
+    inputStr = input.decode();
+    inputStr = inputStr.rstrip()
+    if (inputStr != ''):
+        buttonState = int(inputStr)
+        return buttonState
+    
+
 def loadGunsFromJson(jsonPath):
     with open('guns.json', 'r') as f:
         data = json.load(f)
@@ -27,10 +40,31 @@ def spinMysteryBox(playAudio):
     index = random.randrange(0, len(guns))
     if (playAudio):
         playSound(f"{sfx_path}/mystery-box.wav")
-
+    global prevGun
     prevGun = guns[index]['name']
     print()
     return guns[index]
+
+def spinLimit():
+    return random.randrange(4, 9)
+
+def spawnTeddy():
+    teddy = None
+    for gun in guns:
+        if (gun['name'] == "Teddy Bear"):
+            teddy = gun
+            return teddy
+
+def init():
+    global serialPort
+    serialPort = serial.Serial('COM4', 9600, timeout=1)
+    time.sleep(2)
+    print('====Welcome to Mystery Box Generator=====')
+    print()
+    playSound(f"{sfx_path}/round-begin.wav")
+    global guns 
+    guns = loadGunsFromJson('./guns.json')
+    print('Would you like to spin the box?: (Press button to continue)');
 
 
 def quit():
@@ -39,23 +73,27 @@ def quit():
 
 
 # Main
-playSound(f"{sfx_path}/round-begin.wav")
-guns = loadGunsFromJson('./guns.json')
-
+init()
+spinLimit = spinLimit()
+gunName = ""
 while quitProgram == False:
-    print('Would you like to spin the box?: (Y/N)')
-    answer = input()
-    print()
+    buttonPressed = readButtonState(serialPort)
+    
+    if (buttonPressed):
+        if (spinCount >= spinLimit):
+            gun = spawnTeddy()
+        else: 
+            gun = spinMysteryBox(True)
+            gunName =  gun['name']
+            spinCount += 1
+            while gunName == prevGun:
+                spinMysteryBox(False)
 
-    if (answer == 'y' or answer == 'Y'):
-        gun = spinMysteryBox(True)
-        spinCount += 1
-        while gun['name'] == prevGun:
-            spinMysteryBox(False)
-        print(f"You've gained the {gun['name']}")
+        print(f"You've gained the {gunName}")
         print()
-        if (gun['name'] == "Teddy Bear"):
+    
+        if (gunName == "Teddy Bear"):
             playSound(f"{sfx_path}./teddy.wav")
             quit()
-    else:
-        quit()
+        else:
+            print('Would you like to spin the box?: (Press button to continue)')
