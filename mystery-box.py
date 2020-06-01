4fc8a93c84a0a00f133cb9580db5aab8779edb83
Kieran Brawley <kieran.brawley95@gmail.com>
2020-06-01 11:09:47 +0100

    add expection handling for Serial Port connections

--- mystery-box.py
+++ mystery-box.py
@@ -4,6 +4,7 @@ import json
 import random
 import serial
 import time
+import sys
 
 # Global Variables
 quitProgram = False
@@ -14,15 +15,17 @@ guns = []
 serialPort = None
 
 # Helper methods
-def readButtonState(serialPort): 
-    serialPort.flush() # it is buffering. required to get the data out *now*
-    input = serialPort.readline(100);
-    inputStr = input.decode();
+
+
+def readButtonState(serialPort):
+    serialPort.flush()  # it is buffering. required to get the data out *now*
+    input = serialPort.readline(100)
+    inputStr = input.decode()
     inputStr = inputStr.rstrip()
     if (inputStr != ''):
         buttonState = int(inputStr)
         return buttonState
-    
+
 
 def loadGunsFromJson(jsonPath):
     with open('guns.json', 'r') as f:
@@ -45,8 +48,10 @@ def spinMysteryBox(playAudio):
     print()
     return guns[index]
 
+
 def spinLimit():
     return random.randrange(4, 9)
+
 
 def spawnTeddy():
     teddy = None
@@ -55,16 +60,31 @@ def spawnTeddy():
             teddy = gun
             return teddy
 
-def init():
+
+def readAvailableSerialPorts():
+    import serial.tools.list_ports
+    myports = [tuple(p) for p in list(serial.tools.list_ports.comports())]
+    port: tuple
+    try:
+        port = [port for port in myports if 'COM4' in port][0]
+        port = [port for port in myports if 'COM6' in port][0]
+        print(port)
+    except Exception:
+        print('No available connections were detected')
+        port = None
+    return port
+
+
+def init(availableConnection):
     global serialPort
-    serialPort = serial.Serial('COM4', 9600, timeout=1)
+    serialPort = serial.Serial(availableConnection, 9600, timeout=1)
     time.sleep(2)
     print('====Welcome to Mystery Box Generator=====')
     print()
     playSound(f"{sfx_path}/round-begin.wav")
-    global guns 
+    global guns
     guns = loadGunsFromJson('./guns.json')
-    print('Would you like to spin the box?: (Press button to continue)');
+    print('Would you like to spin the box?: (Press button to continue)')
 
 
 def quit():
@@ -73,25 +93,29 @@ def quit():
 
 
 # Main
-init()
+availableConnection = readAvailableSerialPorts()
+if (availableConnection == None):
+    sys.exit(0)
+
+init(availableConnection)
 spinLimit = spinLimit()
 gunName = ""
 while quitProgram == False:
     buttonPressed = readButtonState(serialPort)
-    
+
     if (buttonPressed):
         if (spinCount >= spinLimit):
             gun = spawnTeddy()
-        else: 
+        else:
             gun = spinMysteryBox(True)
-            gunName =  gun['name']
+            gunName = gun['name']
             spinCount += 1
             while gunName == prevGun:
                 spinMysteryBox(False)
 
         print(f"You've gained the {gunName}")
         print()
-    
+
         if (gunName == "Teddy Bear"):
             playSound(f"{sfx_path}./teddy.wav")
             quit()
